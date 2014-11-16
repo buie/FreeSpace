6f920fef9ac71cfdf3bfd8e70cd4be226be677a5
andrewbuie <buie.andrew@gmail.com>
2014-11-16 07:10:32 -0500

    finished sensor

--- sensor/hackDukeFall14/hackDukeFall14.ino
+++ sensor/hackDukeFall14/hackDukeFall14.ino
@@ -1,42 +1,72 @@
 char server[] = "hackduke.my.to";
 char url[] = "/update.php?room=8";
 int redLed = D0;
-int greenLed = D1;
 int yellowLed = D2;
+int greenLed = D1;
 int pir1 = D4;
 int pir2 = D5;
+
+int pir1State = LOW;
+int pir2State = LOW;
+
+int val1 = 0;
+int val2 = 0;
 
 TCPClient client;
 
 void setup() {
-    Serial.begin(9600);
+    // Serial.begin(9600);
     //init pinmodes
     pinMode(redLed, OUTPUT);
     pinMode(greenLed, OUTPUT);
     pinMode(yellowLed, OUTPUT);
     pinMode(pir1, INPUT);
     pinMode(pir2, INPUT);
-    //write green high by default
-    digitalWrite(greenLed, HIGH);
+    pinMode(D6, INPUT);
+    
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(yellowLed, LOW);
 }
 
 void loop() {
+    // if(digitalRead(D6)==HIGH){
+    //     digitalWrite(D0, HIGH);
+    //     ping();
+    //     delay(10000);
+    //     digitalWrite(D0, LOW);
+    //     delay(1000);
+    // }
     
-    if((digitalRead(pir1) == HIGH) || (digitalRead(pir2) == HIGH)){    //motion detected
-        Serial.println("Starting request");
-        int retval = ping();
-        Serial.print("Returns ");
-        Serial.println(retval);
+    digitalWrite(greenLed, HIGH);
+    delay(2000);
+    digitalWrite(greenLed, LOW);
+    
+    val1 = digitalRead(pir1);
+    val2 = digitalRead(pir2);
+    
+    if ((val1 == HIGH) || (val2 == HIGH)){
         digitalWrite(yellowLed, HIGH);
+        ping();
         delay(10000);
+        if(pir1State == LOW){
+            pir1State = HIGH;
+        }
+        if(pir2State == LOW){
+            pir2State = HIGH;
+        }
     }
     else{
         digitalWrite(yellowLed, LOW);
-        delay(100);
+        if(pir1State == HIGH){
+            pir1State = LOW;
+        }
+        if(pir2State == HIGH){
+            pir2State = LOW;
+        }
     }
 }
-
-int ping(){ //updates room state on server
+void ping(){ //updates room state on server
     client.connect(server, 80);
     if (client.connected()) {
         Serial.println("Connected to server.");
@@ -60,14 +90,14 @@ int ping(){ //updates room state on server
         client.flush();  //for safety
         delay(400);
         client.stop();
-        Serial.print("sent and closed-bytes: ");
-        Serial.println(count);
-    return 1;
+        // Serial.print("sent and closed-bytes: ");
+        // Serial.println(count);
+        return;
      }
     else {
         client.flush();
         client.stop();
-        Serial.println("Not connected");
-        return 0;
+        // Serial.println("Not connected");
+        return;
     }
 }
